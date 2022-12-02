e3b3d896fbb77028a2fc74d6a4610a8f330ce3e8
danielb-cardoso <dbe.cardoso@campus.fct.unl.pt>
2022-12-02 18:55:37 +0000

    a

--- streamlit.py
+++ streamlit.py
@@ -19,5 +19,51 @@ def publish(client):
         print(f"Message sent to topic `{topic}`")
     else:
         print(f"Failed to send message to topic {topic}")
-        
-publish(client)
+
+st.title('Extração de Features de um ficheiro áudio')
+
+if st.button('Start'):        
+    publish(client)
+
+    st.header('Extração de Features')
+    y, sr = librosa.load('grav1.wav') #descarregar o ficheiro
+    
+    # espetrograma
+    st.subheader('Espectograma')
+    
+    X = librosa.stft(y)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(15, 3))
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+    plt.colorbar()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
+    
+    # Magnitude total do sinal (sonoridade / parametro de energia)
+    st.subheader('Sonoridade')
+    
+    # Valor de RMS para cada valor de magnitude
+    S, phase = librosa.magphase(librosa.stft(y)) #frequencia e fase
+    rms = librosa.feature.rms(S=S) #root mean square da gravacao
+    
+    # Grafico
+    fig, ax = plt.subplots(figsize=(15, 6), nrows=2, sharex=True)
+    times = librosa.times_like(rms)
+    ax[0].semilogy(times, rms[0], label='Energia RMS')
+    ax[0].set(xticks=[])
+    ax[0].legend()
+    ax[0].label_outer()
+    librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max),
+                             y_axis='log', x_axis='time', ax=ax[1])
+    ax[1].set(title='Espetrograma da Magnitude logarítmico')
+    st.pyplot()
+    
+    #zero crossing rate
+    st.subheader('Zero Crossing Rate')
+    
+    zcrs = librosa.feature.zero_crossing_rate(y)
+    print(f"Zero crossing rate: {sum(librosa.zero_crossings(y))}")
+    plt.figure(figsize=(15, 3))
+    plt.plot(zcrs[0])
+    plt.title('Zero Crossing Rate')
+    st.pyplot()
